9d800d67e3502fa14aa9317c8436c44138a8ecb6
Tim-HU <timothy999999@gmail.com>
2014-04-03 16:18:34 +0100

    (1) Sample loc with replacement to ensure actual mutation is not less than n_mut due to loc can contain sample value. (2) Implemented insertion along side with mutation.

--- test_wht_noise.m
+++ test_wht_noise.m
@@ -3,6 +3,7 @@ function test_wht_noise()
     % Run `matlabpool open 4` beforehand for faster computation
 
     s = get_fasta_sequence('sequences.fasta',1);
-    wht_noise_study( s(1001:1128), 25 );
+    s_short = s(1:256)
+    wht_noise_study( s_short, 25, 'insert' );
 
 end
--- wht_noise_study.m
+++ wht_noise_study.m
@@ -1,4 +1,4 @@
-function savename = wht_noise_study(seq,rep)
+function savename = wht_noise_study(seq,rep,noise_mode)
 
     % Transform sequence
     seq = seq2num(seq);
@@ -8,42 +8,92 @@ function savename = wht_noise_study(seq,rep)
     order  = nextpow2(seqlen);
     whtlen = pow2(order);
     
-    % Number of repeats
+    % Default value for number of repeats.
+    % Default value for noise_mode.
     if nargin == 1
         rep = ceil(seqlen / 2);
+        noise_mode = 'mutate';
+    elseif nargin == 2
+        noise_mode = 'mutate';
     end
     
     % Precomputations
     seq_wht = dna_wht(seq);
     res     = zeros(4,whtlen,seqlen);
 
-    % For an increasing number of mutations
-    for n_mut = 1:seqlen
+    if strcmp(noise_mode,'mutate')
+
+        % For an increasing number of mutations
+        for n_mut = 1:seqlen
+
+            dif = zeros(4,whtlen,rep);
+
+            % Repeat the experiement rep times
+            for s = 1:rep
+
+                % Create mutated sequence
+                % loc = randi(seqlen,1,n_mut); 
+                % randi changed to randsample because we want sample
+                % without replacement.
+                loc = randsample(seqlen,n_mut);
+                snp = randi(3,1,n_mut);
+                mut = seq;
+                mut(loc) = mod( seq(loc) + snp, 4 );
+
+                % Compute absolute difference of WH coefficients
+                dif(:,:,s) = abs( seq_wht - dna_wht(mut) );
+            end
+
+            % Summarize results
+            res(:,:,n_mut) = (mean( dif, 3 ));        
+            fprintf( '%d/%d mutations done...\n', n_mut, seqlen );
+
+        end
+
+    elseif strcmp(noise_mode,'insert')
+        % Insert increasing number of random individual nucleotides in
+        % sequence, and due to sequence length is fixed, the last 1 bp is
+        % removed at each 1bp insert event.
         
-        dif = zeros(4,whtlen,rep);
-        
-        % Repeat the experiement rep times
-        for s = 1:rep
-            
-            % Create mutated sequence
-            loc = randi(seqlen,1,n_mut);
-            snp = randi(3,1,n_mut);
-            mut = seq;
-            mut(loc) = mod( seq(loc) + snp, 4 );
-            
-            % Compute absolute difference of WH coefficients
-            dif(:,:,s) = abs( seq_wht - dna_wht(mut) );
+        % For an increasing number of inserts
+        for n_mut = 1:seqlen
+
+            dif = zeros(4,whtlen,rep);
+
+            % Repeat the experiement rep times
+            for s = 1:rep
+
+                % Create inserted sequence
+                loc = randsample(seqlen,n_mut);
+                mut = seq;
+                for each_loc = loc
+                    mut((each_loc+1):seqlen) = mut(each_loc:(seqlen-1));
+                    mut(each_loc) = randi(4);
+                    %disp(each_loc);
+                    %disp(mut);
+                end
+
+                % Compute absolute difference of WH coefficients
+                dif(:,:,s) = abs( seq_wht - dna_wht(mut) );
+            end
+
+            % Summarize results
+            res(:,:,n_mut) = (mean( dif, 3 ));        
+            fprintf( '%d/%d insertions done...\n', n_mut, seqlen );
+
         end
         
-        % Summarize results
-        res(:,:,n_mut) = sqrt(mean( dif, 3 ));        
-        fprintf( '%d/%d mutations done...\n', n_mut, seqlen );
+    elseif strcmp(noise_mode,'shift')
+        disp('shift not implemented yet.');
         
+    else
+        disp('Error: noise_mode argumant can only be eith mutate, insert or shift.');
     end
     disp('Done!');
     
+    
     % Save results
-    savename = sprintf('wht_noise_study_%s.mat',datestr(now,'mmmdd-HH:MM:SS'));
+    savename = sprintf('wht_noise_study_%s.mat',datestr(now,'mmmdd-HHMMSS'));
     save(savename,'seq','rep','seq_wht','res');
     
     % Show results
