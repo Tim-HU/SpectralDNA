baeee4aeb17326ec817dae5ddd7fb6460cbf318f
Tim-HU <timothy999999@gmail.com>
2014-04-03 16:10:55 +0100

    Use fastaread instead of importdata for backward compatibility with matlab 2012.

--- get_fasta_sequence.m
+++ get_fasta_sequence.m
@@ -1,8 +1,10 @@
 function seq = get_fasta_sequence( filename, k )
 
     % Load data
-    g = importdata(filename);
-
+    % g = importdata(filename);
+    % importdata works in matlab2013 only. fastaread allows backward
+    % compatibility with matlab2012.
+    g = fastaread(filename); 
     % Apply selector
     if nargin == 1
         glen = length(g);
